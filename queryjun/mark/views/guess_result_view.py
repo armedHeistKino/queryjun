a316c11c3a951ed0987033d2e7937da242ff4c8d
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-08 16:50:24 +0900

    feat: Showing database errors to an user.
    - GuessResultView modified
    	- contionally adds exception message to a response
    
    - MarkingService modified
    	- catching database-raised exceptions
    	- added follwing result type 'ERR'
    	- saving database exception as a data

--- queryjun/mark/views/guess_result_view.py
+++ queryjun/mark/views/guess_result_view.py
@@ -2,11 +2,9 @@ from django import views
 from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render
 
+from ..models import ResultType
 from ..models import GuessResult
-
-from .marking_service import DefaultMarkingService
-from .database_fetcher import PostgresqlFetcher
-from ...submit.models import Guess
+from ..models import GuessResultError
 
 class GuessResultView(views.View):
     """
@@ -20,14 +18,16 @@ class GuessResultView(views.View):
             :param *args:
             :param **kwargs:
         """
-
         guess_result = GuessResult.objects.get(guess_id=self.kwargs['guess_id'])
 
-        context = {
-            'total_execution_time': guess_result.total_execution_time,
-            'result': {
-                'result_acronym': guess_result.result.result_acronym
-            }
-        }
+        context = dict()
+        
+        if guess_result.result.result_acronym == ResultType.objects.get(result_acronym='ERR').result_acronym:
+            error = GuessResultError.objects.get(guess_result_id=guess_result.id)
+
+            context['exception_message'] = error.exception_message
+
+        context['total_execution_time'] = guess_result.total_execution_time
+        context['result'] = { 'result_acronym': guess_result.result.result_acronym }
 
         return JsonResponse(data=context)
--- queryjun/mark/views/marking_service.py
+++ queryjun/mark/views/marking_service.py
@@ -1,6 +1,6 @@
 import time
 
-from ..models import GuessResult, ResultType
+from ..models import GuessResult, ResultType, GuessResultError
 from ...submit.models import Guess
 from ...member.models import Member
 
@@ -15,45 +15,65 @@ class DefaultMarkingService:
         self.guess: Guess = guess
         self.db_fetcher: DatabaseFetcher = db_fetcher
 
-    def _compare(self) -> bool:
+    def _compare(self) -> bool | Exception:
         """
             Compare result from query guess and question answer
             Return True if same 
         """
-        guess_result = self.db_fetcher.fetch_query_result(self.guess)
+        try:
+            guess_result = self.db_fetcher.fetch_query_result(self.guess)
+        except Exception as e:
+            raise e
+        
         answer = self.guess.question.answer
         return str(guess_result) == answer
 
-    def _execute_guess(self) -> tuple[bool, float]:
+    def _execute_guess(self) -> tuple[bool, float] | Exception:
         """
             Get the result of comparison & measure query execution time
         """
         execute_stamp = time.time()
-        is_match = self._compare()
+        try:
+            is_match = self._compare()
+        except Exception as e:
+            raise e
         complete_stamp = time.time()
 
         return is_match, (complete_stamp - execute_stamp) * 1000
     
-    def _decide_result(self, is_match: bool, execute_time) -> ResultType:
+    def _decide_result(self, is_match: bool, execute_time, is_exception_raised: bool=False) -> ResultType:
         """
             Decide which result type should be the guess's mark result
         """
         execute_limit = self.guess.question.execution_limit_milisecond
 
-        if not is_match: # guess did not yield desired result
+        if is_exception_raised: # guess could not be exeucted
+            return ResultType.objects.get(result_acronym='ERR')
+        elif not is_match: # guess did not yield desired result
             return ResultType.objects.get(result_acronym='FL')
         elif execute_time > execute_limit: # guess execution surpassed the limit
             return ResultType.objects.get(result_acronym='OVT')
         else: # clear
             return ResultType.objects.get(result_acronym='CLR')
 
-    def mark(self) -> GuessResult:
+    def mark(self) -> GuessResult | Exception:
         """
             Mark query guess
         """
-        is_match, execute_time = self._execute_guess()
-        
-        result = self._decide_result(is_match, execute_time)
+        exception_flag = False
+
+        try:
+            is_match, execute_time = self._execute_guess()
+        except Exception as e:
+            exception_flag = True
+            exception_message = e
+
+            is_match, execute_time = False, 0.0
+            is_exception_raised = True
+        else:
+            is_exception_raised = False
+
+        result = self._decide_result(is_match, execute_time, is_exception_raised)
 
         guess_result = GuessResult(
             total_execution_time=execute_time,
@@ -62,7 +82,11 @@ class DefaultMarkingService:
         )
         guess_result.save()
 
+        if exception_flag:
+            GuessResultError(
+                exception_message=exception_message,
+                guess_result=guess_result
+            ).save()
+
         if result == ResultType.objects.get(result_acronym='CLR'):
             self.member.solved_question.add(self.guess.question)
-
-        return guess_result
