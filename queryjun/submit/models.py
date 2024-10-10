c06ae82d8a10f887ec3cecbf2bb21c5c5ce80bb8
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-10 20:32:42 +0900

    feat: model update for database selection

--- queryjun/submit/models.py
+++ queryjun/submit/models.py
@@ -1,15 +1,24 @@
+from functools import partial
+
 from django.db import models
-from django.utils import timezone
 
 from ..question.models import Question
 from ..member.models import Member
 
 # Create your models here.
+class VendorOption(models.Model):
+    showing_name = models.CharField(max_length=100)
+    system_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'({str(self.system_name)})'
 
 class Guess(models.Model):
     query_guessed = models.TextField()
     submit_datetime = models.DateTimeField()
     
+    selected_vendor = models.ForeignKey(VendorOption, on_delete=models.PROTECT, default='1')
+
     submitter = models.ForeignKey(Member, on_delete=models.PROTECT)
     question = models.ForeignKey(Question, on_delete=models.PROTECT)
 
