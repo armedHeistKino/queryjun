8cdf1d30b051d22ecc924353ce77f63adddbb6d1
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-10 22:18:51 +0900

    feat: Add attribute for database vendor selection

--- queryjun/submit/forms.py
+++ queryjun/submit/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class GuessSubmitForm(forms.Form):
+    selected_vendor = forms.IntegerField(required=True)
     query_guess = forms.CharField(max_length=2000, required=True)
