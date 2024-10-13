dbf97279bf3ffd5d7f22152115053152f9da2123
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-13 18:21:19 +0900

    style: Remove unneccessary line break for cleaner code

--- queryjun/question/views/question_paginate_mixin.py
+++ queryjun/question/views/question_paginate_mixin.py
@@ -3,5 +3,5 @@ from django.core.paginator import Page, Paginator
 from ..models import Question
 
 class QuesitionPaginateMixin(object):
-    def paginate_question(self, page: int, total_per_page: int = 10) -> Page:
-        return Paginator(Question.objects.all().order_by('id'), total_per_page).get_page(page)
+    def paginate_question(self, page: int, total_per_page: int = 10, latest: bool = False) -> Page:
+        return Paginator(Question.objects.all().order_by(f'{'-' if latest else ''}id'), total_per_page).get_page(page)
