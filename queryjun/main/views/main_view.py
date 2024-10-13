0633199267a2e4d54274f316f07b80b68c826c99
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-13 18:24:49 +0900

    refactor: Apply new pagination mix-ins and update context key name for cleaner code

--- queryjun/main/views/main_view.py
+++ queryjun/main/views/main_view.py
@@ -3,8 +3,9 @@ from django.http import HttpRequest
 from django.shortcuts import render
 
 from ...question.views.question_paginate_mixin import QuesitionPaginateMixin
+from ...workbook.views.workbook_paginate_mixin import WorkbookPaginateMixin
 
-class MainView(QuesitionPaginateMixin, views.View):
+class MainView(QuesitionPaginateMixin, WorkbookPaginateMixin, views.View):
     """
         A CBV for displaying main page
     """
@@ -18,7 +19,8 @@ class MainView(QuesitionPaginateMixin, views.View):
         """
         context = {
             'member': request.user or None,
-            'paginated_question': self.paginate_question(request, 5)
+            'paginated_question': self.paginate_question(page=request.GET.get('p', 1), total_per_page=5, latest=True),
+            'paginated_workbook': self.paginate_workbook(page=request.GET.get('p', 1), total_per_page=5, latest=False),
         }
         
         return render(request, '../templates/main.html', context)
