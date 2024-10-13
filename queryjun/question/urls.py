f88bb178c2140ad3cc4217e37cd4b5c7bc62cc6c
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-13 16:16:24 +0900

    feat: Add new path to question list

--- queryjun/question/urls.py
+++ queryjun/question/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 
+from .views.question_list_view import QuestionListView
 from .views.question_detail_view import QuestionDetailView
 from .views.submit_redirect import submit_redirect
 
 app_name = 'question'
 
 urlpatterns = [
+    path('list/', QuestionListView.as_view(), name='question-list'), 
     path('<int:question_id>/', QuestionDetailView.as_view(), name='question-detail'),
     path('submit-redirect/<int:question_id>/', submit_redirect, name='submit-redirect'),
 ]
