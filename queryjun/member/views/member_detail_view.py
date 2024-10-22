0d9600c7883ab4ec3f671f36a781461eaa2ababd
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-22 17:47:48 +0900

    feat: Implement guess-submit history function
    - Returns QuerySet consists of specific member's submit result.

--- queryjun/member/views/member_detail_view.py
+++ queryjun/member/views/member_detail_view.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 
 from ..models import Member
+from ...mark.models import GuessResult
 from .specific_member_mixin import SpecificMemberMixin
 
 class MemberDetailView(SpecificMemberMixin, TemplateView):
@@ -27,10 +28,13 @@ class MemberDetailView(SpecificMemberMixin, TemplateView):
         except Member.DoesNotExist:
             raise Http404('Member does not exist.')
         
+        submit_history = GuessResult.objects.filter(guess__submitter=request.user).select_related('guess').order_by('-id')[:20]
+        
         solved_question = member.solved_question.all()
         context = { 
             'member': member, 
-            'solved_question': solved_question 
+            'solved_question': solved_question,
+            'submit_history': submit_history,
         }
 
         return render(request, '../templates/member_detail.html', context)
