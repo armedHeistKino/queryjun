73f43bf055605a71a2cc9ffa2d1156d3a5a81582
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-23 15:12:31 +0900

    feat: Add first-start-workbook in context for workbook template

--- queryjun/workbook/views/workbook_list_view.py
+++ queryjun/workbook/views/workbook_list_view.py
@@ -1,6 +1,8 @@
 from django import views
 from django.shortcuts import render
 
+from ...workbook.models import Workbook
+
 from .workbook_paginate_mixin import WorkbookPaginateMixin
 
 class WorkbookListView(WorkbookPaginateMixin, views.View):
@@ -10,9 +12,12 @@ class WorkbookListView(WorkbookPaginateMixin, views.View):
         """
         workbook, empty = self.paginate_workbook_fill_dummy(page=int(request.GET.get('p', 1)), total_per_page=10, latest=False)
 
+        first_start_workbook = Workbook.objects.get(id=1)
+
         context = {
             'paginated_workbook': workbook,
-            'empty': empty
+            'empty': empty,
+            'first_start_workbook': first_start_workbook
         }
 
         return render(request, '../templates/workbook_list.html', context)
