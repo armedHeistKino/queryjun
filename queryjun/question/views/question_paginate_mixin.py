610005db6dfa5f06c98f6584f042b69a8634d00d
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-23 16:29:01 +0900

    fix: Correct to fill dummy properly with pagination

--- queryjun/question/views/question_paginate_mixin.py
+++ queryjun/question/views/question_paginate_mixin.py
@@ -6,7 +6,9 @@ class QuesitionPaginateMixin(object):
     def paginate_question_fill_dummy(self, page: int, total_per_page: int = 10, latest: bool = False) -> tuple[Page, list[None]]:
         questions = Question.objects.all().order_by(f'{'-' if latest else ''}id')
 
-        return Paginator(questions, total_per_page).get_page(page), [None] * (total_per_page - len(questions))
+        page = Paginator(questions, total_per_page).get_page(page)
+
+        return (page, [None] * (total_per_page - len(page.object_list)))
 
     def paginate_question(self, page: int, total_per_page: int = 10, latest: bool = False) -> Page:
         return Paginator(Question.objects.all().order_by(f'{'-' if latest else ''}id'), total_per_page).get_page(page)
--- queryjun/workbook/views/workbook_paginate_mixin.py
+++ queryjun/workbook/views/workbook_paginate_mixin.py
@@ -5,8 +5,10 @@ from ..models import Workbook
 class WorkbookPaginateMixin(object):
     def paginate_workbook_fill_dummy(self, page: int, total_per_page: int = 10, latest: bool = False) -> tuple[Page, list[None]]:
         workbooks = Workbook.objects.all().order_by(f'{'-' if latest else ''}id')
-        
-        return Paginator(workbooks, total_per_page).get_page(page), [None] * (total_per_page - len(workbooks))
+
+        page = Paginator(workbooks, total_per_page).get_page(page)
+
+        return (page, [None] * (total_per_page - len(page.object_list)))
     
     def paginate_workbook(self, page: int, total_per_page: int = 10, latest: bool = False) -> Page:
         return Paginator(Workbook.objects.all().order_by(f'{'-' if latest else ''}id'), total_per_page).get_page(page)
