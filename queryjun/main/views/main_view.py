0b0192b709462d39fd2b8f2ea229a92b37875c6d
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-23 15:07:46 +0900

    feat: Add statics values in context to main template

--- queryjun/main/views/main_view.py
+++ queryjun/main/views/main_view.py
@@ -2,6 +2,9 @@ from django import views
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from ...question.models import Question
+from ...workbook.models import Workbook
+
 from ...question.views.question_paginate_mixin import QuesitionPaginateMixin
 from ...workbook.views.workbook_paginate_mixin import WorkbookPaginateMixin
 
@@ -22,5 +25,10 @@ class MainView(QuesitionPaginateMixin, WorkbookPaginateMixin, views.View):
             'paginated_question': self.paginate_question(page=request.GET.get('p', 1), total_per_page=5, latest=True),
             'paginated_workbook': self.paginate_workbook(page=request.GET.get('p', 1), total_per_page=5, latest=False),
         }
+
+        context['statics'] = [
+            { 'title': '총 문제 수', 'value': Question.objects.count() },
+            { 'title': '총 문제집 수', 'value': Workbook.objects.count() } 
+        ]
         
         return render(request, '../templates/main.html', context)
