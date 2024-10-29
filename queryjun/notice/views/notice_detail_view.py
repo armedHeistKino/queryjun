3492c64f972d4fff2ccc9127539781b5cfeea01a
armedHeistKino <urban.manul.1919@gmail.com>
2024-10-29 18:02:01 +0900

    feat: Add a view for notice detail

---
+++ queryjun/notice/views/notice_detail_view.py
@@ -0,0 +1,13 @@
+from django import views
+from django.shortcuts import render
+from django.http import HttpRequest
+
+from ..models import Notice
+
+class NoticeDetailView(views.View):
+    def get(self, request: HttpRequest, *args, **kwargs):
+        context = { 
+            'notice': Notice.objects.get(id=kwargs['notice_id']) 
+        }
+        
+        return render(request, '../templates/notice-detail.html', context)
