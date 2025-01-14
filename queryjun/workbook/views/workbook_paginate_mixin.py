d195f95c04c5940d848dcfd82faba8e15f0afa60
armedHeistKino <urban.manul.1919@gmail.com>
2025-01-14 14:25:16 +0900

    fix: Change f-string quotation marks to double

--- queryjun/workbook/views/workbook_paginate_mixin.py
+++ queryjun/workbook/views/workbook_paginate_mixin.py
@@ -4,11 +4,11 @@ from ..models import Workbook
 
 class WorkbookPaginateMixin(object):
     def paginate_workbook_fill_dummy(self, page: int, total_per_page: int = 10, latest: bool = False) -> tuple[Page, list[None]]:
-        workbooks = Workbook.objects.all().order_by(f'{'-' if latest else ''}id')
+        workbooks = Workbook.objects.all().order_by(f"{'-' if latest else ''}id")
 
         page = Paginator(workbooks, total_per_page).get_page(page)
 
         return (page, [None] * (total_per_page - len(page.object_list)))
     
     def paginate_workbook(self, page: int, total_per_page: int = 10, latest: bool = False) -> Page:
-        return Paginator(Workbook.objects.all().order_by(f'{'-' if latest else ''}id'), total_per_page).get_page(page)
+        return Paginator(Workbook.objects.all().order_by(f"{'-' if latest else ''}id"), total_per_page).get_page(page)
