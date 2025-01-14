67e0187f9a1faebdca94bebd0603ad96aa027a67
armedHeistKino <urban.manul.1919@gmail.com>
2025-01-14 14:18:11 +0900

    fix: Change f-string quotation marks to double
    - f-string wrapping quoatation marks changed from single to double
    
    Single quotation marks wrapped by single quotation marks raises exception
    in Gunicorn environment. I made a silly mistake.

--- queryjun/question/views/question_paginate_mixin.py
+++ queryjun/question/views/question_paginate_mixin.py
@@ -4,11 +4,11 @@ from ..models import Question
 
 class QuesitionPaginateMixin(object):
     def paginate_question_fill_dummy(self, page: int, total_per_page: int = 10, latest: bool = False) -> tuple[Page, list[None]]:
-        questions = Question.objects.all().order_by(f'{'-' if latest else ''}id')
+        questions = Question.objects.all().order_by(f"{'-' if latest else ''}id")
 
         page = Paginator(questions, total_per_page).get_page(page)
 
         return (page, [None] * (total_per_page - len(page.object_list)))
 
     def paginate_question(self, page: int, total_per_page: int = 10, latest: bool = False) -> Page:
-        return Paginator(Question.objects.all().order_by(f'{'-' if latest else ''}id'), total_per_page).get_page(page)
+        return Paginator(Question.objects.all().order_by(f"{'-' if latest else ''}id"), total_per_page).get_page(page)
