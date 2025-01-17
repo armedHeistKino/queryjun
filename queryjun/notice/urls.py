20c21cfe44a4b8027070adaf713582ee0bcb0eb1
armedHeistKino <urban.manul.1919@gmail.com>
2025-01-17 10:36:41 +0900

    feat: Add url pattern for notice list

--- queryjun/notice/urls.py
+++ queryjun/notice/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from .views.notice_list_view import NoticeListView
 from .views.notice_detail_view import NoticeDetailView
 
 urlpatterns = [
+    path('', NoticeListView.as_view(), name='notice-list'),
     path('<int:notice_id>/', NoticeDetailView.as_view(), name='notice-detail'),
 ]
