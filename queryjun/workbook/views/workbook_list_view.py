badb9c4d45f3093ea314df5f22beec24d9e23769
armedHeistKino <urban.manul.1919@gmail.com>
2025-01-17 10:41:03 +0900

    refactor: Remove getting first workbook
    - Remove `Workbook.object.get(id=1)`
    
    Have found that paginating functions are incohesive, spreading over the
    code base. Furthermore, paginating and its applications are nosensical,
    being potential code smell in the long run.

--- queryjun/workbook/views/workbook_list_view.py
+++ queryjun/workbook/views/workbook_list_view.py
@@ -12,12 +12,9 @@ class WorkbookListView(WorkbookPaginateMixin, views.View):
         """
         workbook, empty = self.paginate_workbook_fill_dummy(page=int(request.GET.get('p', 1)), total_per_page=10, latest=False)
 
-        first_start_workbook = Workbook.objects.get(id=1)
-
         context = {
             'paginated_workbook': workbook,
             'empty': empty,
-            'first_start_workbook': first_start_workbook
         }
 
         return render(request, '../templates/workbook_list.html', context)
