e67bc70302da6d003e01f78dff873b36bd307c8f
armedHeistKino <urban.manul.1919@gmail.com>
2025-01-17 10:37:10 +0900

    chore: Expose notice url pattern to external

--- queryjun/notice/urls.py
+++ queryjun/notice/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views.notice_list_view import NoticeListView
 from .views.notice_detail_view import NoticeDetailView
 
+app_name = 'notice'
+
 urlpatterns = [
     path('', NoticeListView.as_view(), name='notice-list'),
     path('<int:notice_id>/', NoticeDetailView.as_view(), name='notice-detail'),
